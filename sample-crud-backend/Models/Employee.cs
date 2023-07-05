296fa5ed432b1c4416a323484763a7413b05bab8
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2023-07-05 14:53:32 +0100

    first commit

---
+++ sample-crud-backend/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace sample_crud_backend.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string IsActive { get; set; }
+    }
+}
+   
---
+++ sample-crud-backend/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sample_crud_backend.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext( DbContextOptions<EmployeeContext> opions) : base(opions)
+        {
+           
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
